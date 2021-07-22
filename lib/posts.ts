057e05dcd98554680fd86374fb99f9b589623ddc
smilejin92 <smilejin92@gmail.com>
2021-07-22 21:45:30 +0900

    [feat] getPostData 함수 수정

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import remark from 'remark';
+import html from 'remark-html';
 
 interface PostYml {
   title: string;
@@ -9,17 +11,24 @@ interface PostYml {
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-export function getPostData(id: string) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
 
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
   // Combine the data with the id
   return {
     id,
     ...(matterResult.data as PostYml),
+    contentHtml,
   };
 }
 
