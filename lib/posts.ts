2494f408ba0a560d8919afceadcad7416e18d199
smilejin92 <smilejin92@gmail.com>
2021-07-22 20:16:45 +0900

    [feat] api 추가
    - getPostData
    - getAllPostIds

--- lib/posts.ts
+++ lib/posts.ts
@@ -9,6 +9,45 @@ interface PostYml {
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
+export function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the id
+  return {
+    id,
+    ...(matterResult.data as PostYml),
+  };
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    };
+  });
+}
+
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
