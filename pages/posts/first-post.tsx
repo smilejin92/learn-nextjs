82d2c393462fbcd217d6b01f9538875fcf5926f6
smilejin92 <smilejin92@gmail.com>
2021-07-22 15:23:43 +0900

    [feat] Layout 컴포넌트 적용

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,18 +1,13 @@
-import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/Layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout home={false}>
       <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
+    </Layout>
   );
 }
