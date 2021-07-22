1c8102ce12ee6c14782553dffb7f3aa191cb4a40
smilejin92 <smilejin92@gmail.com>
2021-07-22 22:36:57 +0900

    [feat] Home 컴포넌트 JSX 마크업 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,10 +35,6 @@ export default function Home({ allPostsData }: HomeProps) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Next.js를 열심히 공부해보자</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
